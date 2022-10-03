091c00ad52429a7a349ffe3524f676aa50d7cb8b
MartinDap <jorge.del.aguila@upeu.edu.pe>
2022-10-03 17:48:46 -0500

    subiendo mi proyecto spring

---
+++ src/main/java/com/delaguila/demo/MdelaguilaApplication.java
@@ -0,0 +1,13 @@
+package com.delaguila.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdelaguilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdelaguilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delaguila/demo/MdelaguilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delaguila.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdelaguilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
